16ee1d075e69abda52327d8766c6e19a160ee380
Leo-Dell-Ubuntu <LeoJhon.Song@outlook.com>
2019-11-27 00:26:36 +0800

    feat(workflow): migrate CI from travis to github action

---
+++ themes/uestc-msc/asset/post_asset.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const { Schema } = require('warehouse');
+const { join } = require('path');
+// const Schema = require('warehouse').Schema;
+
+module.exports = ctx => {
+  const PostAsset = new Schema({
+    _id: {type: String, required: true},
+    slug: {type: String, required: true},
+    modified: {type: Boolean, default: true},
+    post: {type: Schema.Types.CUID, ref: 'Post'},
+    renderable: {type: Boolean, default: true}
+  });
+
+  PostAsset.virtual('path').get(function() {
+    const Post = ctx.model('Post');
+    const post = Post.findById(this.post);
+    if (!post) return;
+
+    // PostAsset.path is file path relative to `public_dir`
+    // no need to urlescape, #1562
+    // strip /\.html?$/ extensions on permalink, #2134
+    // return pathFn.join(post.path.replace(/\.html?$/, ''), post.path.replace(/\.html?$/, ''), this.slug);
+    return join(post.path.replace(/\.html?$/, ''), (post.path.replace(/\.html?$/, '')).substr((post.path.replace(/\.html?$/, '')).lastIndexOf(RegExp("\\d\\d\\d\\d/\\d\\d/\\d\\d")) + 11), this.slug);
+  });
+
+  PostAsset.virtual('source').get(function() {
+    return join(ctx.base_dir, this._id);
+  });
+
+  return PostAsset;
+};
